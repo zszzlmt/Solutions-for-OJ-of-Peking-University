7fee73c485e84915b4a4e31e8ef913d391edf1b6
张世泽 <814135975@qq.com>
2017-05-07 22:35:44 +0800

    unfinished #2488

---
+++ 2488/2488/2488.cpp
@@ -0,0 +1,45 @@
+/*******************************************************************************
+** Copyright (C), 	2017- , Zach Yeo.                                          *
+** File name:		2488.cpp                                                   *
+** Description:                                                                *
+** Author:			Zhang Shize <zszv587@gmail.com>                            *
+**-----------------------------------------------------------------------------*
+** History:                                                                    *
+** v1.0				2017-05-07					                               *
+********************************************************************************/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+
+using namespace std;
+
+/*number * letters */
+int iStep[ 8 ] = { -1, 1, -2, 2, -2, 2, -1, 1 };
+int jStep[ 8 ] = { -2, -2, -1, -1, 1, 1, 2, 2 };
+
+bool map[ 26 + 1 ][ 26 + 1 ] = { 0 };
+
+int main ( ){
+
+	int nCase;
+	int k;
+	int row;
+	int col;
+	int i;
+	int j;
+
+	cin >> nCase;
+
+	for ( k = 1; k <= nCase; k ++ ){
+	
+		cout << "Scenario #" << k << ":" << endl;
+		cin >> row >> col;
+
+
+	
+	}
+
+	return 0;
+
+}
